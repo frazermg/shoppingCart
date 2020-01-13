f54a35dd57095ff5f93b6e1e7c5cab3720f96642
frazermg <mgriffiths29@gmail.com>
2020-01-12 19:57:01 -0800

    product item

---
+++ constants/Colors.js
@@ -0,0 +1,4 @@
+export default {
+  primary: "#374C1E",
+  accent: "#FFC10F"
+};
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { FlatList, StyleSheet } from "react-native";
+import { Text, FlatList, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
+
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
-  return <FlatList data={products} keyExtractor={products.id} />;
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      // !! finish off product item props
+      renderItem={itemData => <ProductItem />}
+    />
+  );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: "All Products"
 };
 
 const styles = StyleSheet.create({});
