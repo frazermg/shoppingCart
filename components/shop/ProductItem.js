5a6f8cb0f0b88200e94d6a867c54dbce13741fa0
frazermg <mgriffiths29@gmail.com>
2020-04-23 20:18:34 -0700

    product item

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,15 +1,51 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
+
+// ** TouchableNativeFeedback allows us to add better feedback on touch events
+
+import Colors from "../../constants/Colors";
 
 const ProductItem = ({ image, title, price, onViewDetail, onAddToCart }) => {
+  let TouchableCpm = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableCpm = TouchableNativeFeedback;
+  }
   return (
     <View style={styles.product}>
-      <Image style={styles.image} source={{ uri: image }} />
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.price}>{price.toFixed(2)}</Text>
-      <View style={styles.actions}>
-        <Button title="View Details" onPress={onViewDetail} />
-        <Button title="To Cart" onPress={onAddToCart} />
+      <View style={styles.touchable}>
+        <TouchableCpm onPress={onViewDetail} useForeground>
+          <View>
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={{ uri: image }} />
+            </View>
+            <View style={styles.details}>
+              <Text style={styles.title}>{title}</Text>
+              <Text style={styles.price}>{price.toFixed(2)}</Text>
+            </View>
+            <View style={styles.actions}>
+              <Button
+                color={Colors.primary}
+                title="View Details"
+                onPress={onViewDetail}
+              />
+              <Button
+                color={Colors.primary}
+                title="To Cart"
+                onPress={onAddToCart}
+              />
+            </View>
+          </View>
+        </TouchableCpm>
       </View>
     </View>
   );
@@ -25,25 +61,45 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: "white",
     height: 300,
-    margin: 20
+    margin: 20,
+    overflow: "hidden",
+  },
+  touchable: {
+    overflow: "hidden",
+    borderRadius: 10,
+  },
+
+  imageContainer: {
+    width: "100%",
+    height: "60%",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    overflow: "hidden",
   },
   image: {
     width: "100%",
-    height: "60%"
+    height: "100%",
   },
   title: {
     fontSize: 18,
-    marginVertical: 4
+    marginVertical: 4,
   },
   price: {
     fontSize: 14,
-    color: "#888"
+    color: "#888",
+  },
+  details: {
+    alignItems: "center",
+    height: "15%",
+    padding: 10,
   },
   actions: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignContent: "center"
-  }
+    alignItems: "center",
+    height: "25%",
+    paddingHorizontal: 20,
+  },
 });
 
 export default ProductItem;
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -4,20 +4,32 @@ import { useSelector } from "react-redux";
 
 import ProductItem from "../../components/shop/ProductItem";
 
-const ProductsOverviewScreen = props => {
-  const products = useSelector(state => state.products.availableProducts);
+const ProductsOverviewScreen = ({ navigation }) => {
+  const products = useSelector((state) => state.products.availableProducts);
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
-      // !! finish off product item props
-      renderItem={itemData => <ProductItem />}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() =>
+            navigation.navigate("ProductDetail", {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title,
+            })
+          }
+          onAddToCart={() => {}}
+        />
+      )}
     />
   );
 };
 
 ProductsOverviewScreen.navigationOptions = {
-  headerTitle: "All Products"
+  headerTitle: "All Products",
 };
 
 const styles = StyleSheet.create({});
